c23a6fa5893d196801b5c9bbddf14c9b04030d36
Elham Aryanpur <elhamaryanpur5@gmail.com>
2021-08-12 20:11:37 +0300

    Structure's base is built and is ready to start adding features

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,32 @@
+use anyhow::Result;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Gigantesque {
+    name: String,
+    on_memory: Option<String>,
+}
+
+impl Gigantesque {
+    pub fn new(name: &str, on_memory: bool) -> Result<Self> {
+        Ok(Self {
+            name: format!("{}.gigantesque", name),
+            on_memory: if on_memory { Some(String::new()) } else { None },
+        })
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::Gigantesque;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn creation() {
+        assert_eq!(
+            Gigantesque::new("name", true).unwrap(),
+            Gigantesque {
+                name: String::from("name.gigantesque"),
+                on_memory: Some(String::new())
+            }
+        );
     }
 }
